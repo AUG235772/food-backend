8d9cb7b901313b0c81888ed4bc8d3fe370155595
AUG235772.com <aug235772@gmail.com>
2025-01-25 17:10:31 +0530

    Update MongoDB connection URL

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: '6b30fc10f14ee5a6d87f01bc287b7cc66cead14c21b8544e989d5b0beb882bc3fd799a17125b64dbbe9733c7546fe1d3a71e31e0e0438890bd4fb678c57fb701',
-    mongoURI: 'mongodb+srv://aug235772:R!PnCuW65ih.@5m@cluster0.k45ng.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    mongoURI: 'mongodb+srv://aug235772: R!PnCuW65ih.@5m @cluster0.k45ng.mongodb.net/<dbname>?retryWrites=true&w=majority'
 };

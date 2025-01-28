8c7baba7315da679d9c64b42e6c037deb7f89b31
AUG235772.com <aug235772@gmail.com>
2025-01-28 20:29:56 +0530

    Fix path to Firebase service account JSON file

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../path/to/your/servesmart-services-firebase-adminsdk-fbsvc-3c580ec9a1.json'); // Download this from Firebase Console
+const serviceAccount = require('./servesmart-services-firebase-adminsdk-fbsvc-3c580ec9a1.json'); // Ensure this path is correct
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
